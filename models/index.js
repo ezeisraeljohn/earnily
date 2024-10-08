d03dd34dd11a409989efc96f2a46185b97acbe7d
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-08 11:59:57 +0100

    Add application, job, and user models

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const Job = require("./job_model");
+const Application = require("./application_model");
+const User = require("./user_model");
+
+module.exports = {
+  User,
+  Job,
+  Application,
+};
