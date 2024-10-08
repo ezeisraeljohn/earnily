03b2c170804271537a6b2d10564398911b17d2f1
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-08 22:07:16 +0100

    Refactor job_controller.js to add createJob and updateJob routes with authentication and authorization

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -1,6 +1,13 @@
 const Job = require("../models/index").Job;
-const Application = require("../models/index").Application;
 
+/**
+ * @desc Create a new job
+ * @param {Object} req - Request object
+ * @param {Object} res - Response object
+ * @route POST /api/v1/jobs
+ * @access Private
+ * @returns {Promise<void>}
+ */
 const createJob = async (req, res) => {
   try {
     const { title, description, location, jobType, salary, company } = req.body;
@@ -29,4 +36,45 @@ const createJob = async (req, res) => {
   }
 };
 
-module.exports = { createJob };
+/**
+ * @desc Update a Job
+ *@param {Object} req - Request object
+ *@param {Object} res - Response object
+ *@route PUT /api/v1/jobs/:id
+ *@access Private
+ *@returns {Promise<void>}
+ */
+const updateJob = async (req, res) => {
+  try {
+    let job = await Job.findById(req.params.id);
+    if (!job) {
+      return res.status(404).json({
+        success: false,
+        status: 404,
+        message: "Job not found",
+      });
+    }
+    if (job.postedBy.toString() !== req.user.id) {
+      return res.status(401).json({
+        success: false,
+        status: 401,
+        message: "You are not authorized to update this job",
+      });
+    }
+    job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      success: true,
+      status: 200,
+      message: "Job updated successfully",
+      data: job,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+module.exports = { createJob, updateJob };
