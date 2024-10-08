37e9d5306348a2c81206341feda769cf2dfa35b4
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-08 13:27:37 +0100

    Add authentication routes to server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const connectDB = require("./utils/db");
 const express = require("express");
+const authRouter = require("./routes/auth_routes");
 
 const app = express();
+
 connectDB();
+
+app.use("/api/v1", authRouter);
