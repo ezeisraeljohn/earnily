d228f244d5adf29df57b61cfe56f2db312d7604f
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-08 18:42:05 +0100

    Refactor server.js to add routes for creating jobs with authentication and authorization

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const connectDB = require("./utils/db");
 const express = require("express");
 const authRouter = require("./routes/auth_routes");
+const jobsRouter = require("./routes/job_routes");
 
 const app = express();
 
 connectDB();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", authRouter);
+app.use("/api/v1", jobsRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
