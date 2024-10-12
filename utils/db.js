d745a31782cfe2a92fffbb8b6467be9639543c7e
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-12 23:05:11 +0100

    Refactor db.js to use environment variable for MongoDB connection string

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,15 @@
 const moongose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await moongose.connect("mongodb://localhost:27017/jobsearch");
+    const MONGO_URI =
+      process.env.MONGO_URL || "mongodb://localhost:27017/jobsearch";
+    const conn = await moongose.connect(MONGO_URI, {
+      serverSelectionTimeoutMS: 60000,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.log(err);
