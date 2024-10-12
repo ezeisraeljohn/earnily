aa785dedcd9f796959bacfdc0f25333c8ec974ff
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-12 01:55:58 +0100

    Add file upload functionality and application routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const connectDB = require("./utils/db");
 const express = require("express");
 const authRouter = require("./routes/auth_routes");
 const jobsRouter = require("./routes/job_routes");
+const applicationRouter = require("./routes/application_route");
+const path = require("path");
 
 const app = express();
 
@@ -9,6 +11,8 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/v1", applicationRouter);
 app.use("/api/v1", authRouter);
 app.use("/api/v1", jobsRouter);
 
