cede8824196127881f80307d1b0b15c9ad527747
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-12 01:55:25 +0100

    Refactor job_routes.js to import createJob, updateJob, deleteJob, getJobs, and getJob functions from job_controller.js and update import statements

--- routes/job_routes.js
+++ routes/job_routes.js
@@ -1,7 +1,16 @@
-const { createJob } = require("../controllers/job_controller");
+const {
+  createJob,
+  updateJob,
+  deleteJob,
+  getJobs,
+  getJob,
+} = require("../controllers/job_controller");
 const { protect, authorize } = require("../middlewares/auth_middleware");
 const router = require("express").Router();
 
 router.post("/jobs", protect, authorize("employer"), createJob);
-
+router.put("/jobs/:id", protect, authorize("employer"), updateJob);
+router.delete("/jobs/:id", protect, authorize("employer"), deleteJob);
+router.get("/jobs", protect, authorize("employer"), getJobs);
+router.get("/jobs/:id", protect, authorize("employer"), getJob);
 module.exports = router;
