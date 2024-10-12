5fdff573d8ba055739c9e335ac2c90420ae195bc
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-12 01:55:00 +0100

    Refactor job_controller.js to update updateJob route and add deleteJob, getJobs, and getJob routes with authentication and authorization

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -65,11 +65,14 @@ const updateJob = async (req, res) => {
       new: true,
       runValidators: true,
     });
+    const { __v: none, ...jobWithoutV } = job.toObject();
+    jobWithoutV.id = jobWithoutV._id;
+    delete jobWithoutV._id;
     res.status(200).json({
       success: true,
       status: 200,
       message: "Job updated successfully",
-      data: job,
+      data: jobWithoutV,
     });
   } catch (error) {
     console.error(error.message);
@@ -77,4 +80,72 @@ const updateJob = async (req, res) => {
   }
 };
 
-module.exports = { createJob, updateJob };
+/**
+ * @desc Delete a Job
+ * @param {Object} req - Request object
+ * @param {Object} res - Response object
+ * @route DELETE /api/v1/jobs/:id
+ * @access Private
+ * @returns {Promise<void>}
+ */
+const deleteJob = async (req, res) => {
+  try {
+    let job = await Job.findById(req.params.id);
+    if (!job) {
+      return res.status(404).json({
+        success: false,
+        status: 404,
+        message: "Job not found",
+      });
+    }
+    if (job.postedBy.toString() !== req.user.id) {
+      return res.status(401).json({
+        success: false,
+        status: 401,
+        message: "You are not authorized to delete this job",
+      });
+    }
+    await Job.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      success: true,
+      status: 200,
+      message: "Job deleted successfully",
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+const getJobs = async (req, res) => {
+  try {
+    const jobs = await Job.find({ postedBy: req.user.id });
+    res.status(200).json({ success: true, status: 200, data: jobs });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+const getJob = async (req, res) => {
+  try {
+    const job = await Job.findById(req.params.id);
+    if (job.postedBy.toString() !== req.user.id) {
+      return res.status(401).json({
+        success: false,
+        status: 401,
+        message: "You are not authorized to view this job",
+      });
+    }
+    const new_job = job.toObject();
+    new_job.id = new_job._id;
+    delete new_job._id;
+    delete new_job.__v;
+    res.status(200).json({ success: true, status: 200, data: new_job });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+module.exports = { createJob, updateJob, deleteJob, getJobs, getJob };
