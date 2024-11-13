3acac09554a8601dfdd530304acfe75c92c622f2
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-11-13 02:49:37 +0100

    Refactor database connection logic for test environment

--- utils/db.js
+++ utils/db.js
@@ -5,11 +5,18 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const MONGO_URI = process.env.TEST_MONGO_URL || process.env.MONGO_URL;
+    let MONGO_URI;
+    if (process.env.NODE_ENV === "test") {
+      MONGO_URI = process.env.T_MONGO_URL;
+    } else {
+      MONGO_URI = process.env.TEST_MONGO_URL || process.env.MONGO_URL;
+    }
     const conn = await moongose.connect(MONGO_URI, {
       serverSelectionTimeoutMS: 60000,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    if (process.env.NODE_ENV !== "test") {
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    }
   } catch (err) {
     console.log(err);
     process.exit(1);
