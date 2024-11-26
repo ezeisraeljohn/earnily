f544844ddf6ba914feaadc4e43073fb8e6f70a09
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-11-26 12:32:14 +0100

    Add job and user test data utility functions for testing purposes

---
+++ utils/user_test_data.js
@@ -0,0 +1,12 @@
+// Returns the object that will be used for testing
+const user_detail = () => {
+  return {
+    firstName: "john",
+    lastName: "Israel",
+    role: "employer",
+    email: "ezeisrael2wqw@gmail.com",
+    password: "2233223322",
+  };
+};
+
+module.exports = user_detail;
