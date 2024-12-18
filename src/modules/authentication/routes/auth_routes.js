2fbd46da39d6d0ec370cbea34cef85353f5451f4
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-12-18 04:55:10 +0100

    Add: implement email verification route with protection middleware

--- src/modules/authentication/routes/auth_routes.js
+++ src/modules/authentication/routes/auth_routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { register, login } = require("../controllers/auth_controller");
+const { protect } = require("../../../shared/middlewares/auth_middleware");
+const {
+  register,
+  login,
+  activateEmail,
+} = require("../controllers/auth_controller");
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/verify-email", protect, activateEmail);
 router.post("/login", login);
 
 module.exports = router;
