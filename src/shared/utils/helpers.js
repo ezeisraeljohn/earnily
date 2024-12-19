e461e278621cf5ef78961a5940775e4c549a7da3
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-12-19 18:57:24 +0100

    Add: implement generateSalt function for bcrypt salt generation

--- src/shared/utils/helpers.js
+++ src/shared/utils/helpers.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const generateOtp = (num) =>
   Math.floor(
     Math.random() * (9 * Math.pow(10, num - 1)) + Math.pow(10, num - 1)
@@ -8,4 +9,8 @@ const generateToken = (payload) => {
   const token = jwt.sign(payload, process.env.SECRET, { expiresIn: "2d" });
   return token;
 };
-module.exports = { generateOtp, generateToken };
+
+const generateSalt = (value) => {
+  return bcrypt.genSaltSync(value);
+};
+module.exports = { generateOtp, generateToken, generateSalt };
