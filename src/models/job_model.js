6f85ba85078d8ac2be4b429efc093a1eefcc2136
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-12-19 21:47:55 +0100

    Refactor: streamline job schema definition for improved readability

--- src/models/job_model.js
+++ src/models/job_model.js
@@ -1,30 +1,13 @@
 const mongoose = require("mongoose");
 
 const jobSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Please provide a title"],
-  },
-  company: {
-    type: String,
-    requrired: [true, "Please provide a company"],
-  },
-  location: {
-    type: String,
-    required: [true, "Please provide a location"],
-  },
-  salaryMin: {
-    type: Number,
-    required: [true, "Please provide a salary"],
-  },
-  salaryMax: {
-    type: Number,
-    required: [true, "Please provide a salary"],
-  },
+  title: { type: String, required: [true, "Please provide a title"] },
+  company: { type: String, requrired: [true, "Please provide a company"] },
+  location: { type: String, required: [true, "Please provide a location"] },
+  salaryMin: { type: Number, required: [true, "Please provide a salary"] },
+  salaryMax: { type: Number, required: [true, "Please provide a salary"] },
 
-  description: {
-    type: String,
-  },
+  description: { type: String },
   jobType: {
     type: String,
     enum: ["full-time", "part-time", "contract", "temporary", "internship"],
@@ -35,10 +18,7 @@ const jobSchema = new mongoose.Schema({
     ref: "User",
     required: [true, "Please provide a user (only employers can post jobs)"],
   },
-  datePosted: {
-    type: Date,
-    default: Date.now,
-  },
+  datePosted: { type: Date, default: Date.now },
 });
 
 const Job = mongoose.model("Job", jobSchema);
