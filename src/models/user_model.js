90f76e08751636bc9466f646fd1f641efbfe2c22
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-12-20 13:16:20 +0100

    Add: enhance user schema with password reset fields and maintain timestamps

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -1,61 +1,25 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String },
+    lastName: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["employer", "jobseeker"], required: true },
+    resume: { type: String, required: false },
+    date: { type: Date, default: Date.now },
+    isActive: { type: Boolean, default: true },
+    profilePicture: { type: String, required: false },
+    isEmailVerified: { type: Boolean, default: false },
+    emailVerificationToken: { type: String, required: false },
+    emailVerifiedAt: { type: Date, required: false },
+    isAdmin: { type: Boolean, required: false, default: false },
+    isPasswordReset: { type: Boolean, required: false, default: false },
+    isPasswordResetExpiredAt: { type: Date, requrired: false },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["employer", "jobseeker"],
-    required: true,
-  },
-  resume: {
-    type: String,
-    required: false,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-  profilePicture: {
-    type: String,
-    required: false,
-  },
-  isEmailVerified: {
-    type: Boolean,
-    default: false,
-  },
-  emailVerificationToken: {
-    type: String,
-    required: false,
-  },
-  emailVerifiedAt: {
-    type: Date,
-    required: false,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: false,
-    default: false,
-  },
-  timestamps: true,
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
