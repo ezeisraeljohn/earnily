dcf2ae2d3f916d2de734e6b607577a5fa7f51d7d
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-12-22 02:01:19 +0100

    feat: add device and location tracking functionality

--- src/shared/utils/helpers.js
+++ src/shared/utils/helpers.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const UAParser = require("ua-parser-js");
 const bcrypt = require("bcryptjs");
 const generateOtp = (num) =>
   Math.floor(
@@ -13,4 +14,50 @@ const generateToken = (payload) => {
 const generateSalt = (value) => {
   return bcrypt.genSaltSync(value);
 };
-module.exports = { generateOtp, generateToken, generateSalt };
+
+const getIPAddress = (req) => {
+  return (req.headers["x-forwarded-for"] || req.connection.remoteAddress).split(
+    ","
+  )[0];
+};
+
+const getLocation = async (ipAddress) => {
+  const response = await fetch(
+    `${process.env.IPINFO_URL}/${ipAddress}?token=${process.env.IPINFO_TOKEN}`
+  );
+  const location = await response.json();
+  return location;
+};
+
+const getDevice = (device) => {
+  const parser = new UAParser();
+  parser.setUA(device);
+  return parser.getResult();
+};
+
+const getDeviceAndLocation = async (req) => {
+  const device = req.headers["user-agent"];
+  const newDevice = getDevice(device);
+  const ipAddress = getIPAddress(req);
+  const location = await getLocation(ipAddress);
+
+  return {
+    device: `${newDevice.device?.vendor || "Unknown"} ${
+      newDevice.device?.model || "Unknown"
+    }`,
+    os: newDevice.os?.name || "Unknown",
+    browser: newDevice.browser?.name || "Unknown",
+    location: {
+      city: location.city,
+      region: location.region,
+      country: location.country,
+    },
+  };
+};
+module.exports = {
+  generateOtp,
+  generateToken,
+  generateSalt,
+  getIPAddress,
+  getDeviceAndLocation,
+};
