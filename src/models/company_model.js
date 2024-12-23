248b421e113a2b0c6a738823a134bee3e49a6a3b
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-12-23 13:42:19 +0100

    feat: add company model with schema definition

---
+++ src/models/company_model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+    },
+    logo: {
+      type: String,
+    },
+    createdBy: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "please enter the user id"],
+    },
+    dateCreated: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const Company = mongoose.model("Company", companySchema);
+module.exports = Company;
