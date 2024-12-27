24007d14a38a443eb5be21e295ab53f46b4dea91
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-12-27 18:15:17 +0100

    feat: add review routes for job and company reviews in server configuration

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const jobsRouter = require("./src/modules/jobs/routes/job_routes");
 const applicationRouter = require("./src/modules/application/routes/application_route");
 const userRouter = require("./src/modules/user/routes/user_routes");
 const companyRouter = require("./src/modules/companies/routes/company_routes");
+const ReviewJobRouter = require("./src/modules/reviews/job_reviews/routes/job_review_routes");
+const ReviewCompanyRouter = require("./src/modules/reviews/company_review/routes/company_review_routes");
 const path = require("path");
 const cors = require("cors");
 const rateLimiter = require("./src/shared/utils/ratelimiting");
@@ -21,9 +23,9 @@ app.use(rateLimiter);
 app.use("/api/v1/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/company/", companyRouter);
+app.use("/api/v1/company", companyRouter, ReviewCompanyRouter);
 app.use("/api/v1", applicationRouter);
-app.use("/api/v1", jobsRouter);
+app.use("/api/v1/jobs", jobsRouter, ReviewJobRouter);
 const PORT = process.env.PORT || 8080;
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
