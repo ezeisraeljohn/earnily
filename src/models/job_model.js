4e2ce8ecbf6cf03371c87bce5d30a702bbcbfd6b
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-12-28 13:38:51 +0100

    feat: enhance job model with additional fields for openings, experience level, duration, status, and expiry date

--- src/models/job_model.js
+++ src/models/job_model.js
@@ -17,12 +17,27 @@ const jobSchema = new mongoose.Schema(
       enum: ["full-time", "part-time", "contract", "temporary", "internship"],
       required: [true, "Please provide a job type"],
     },
+    numberOfOpenings: { type: Number, default: 1 },
     postedBy: {
       type: mongoose.Schema.ObjectId,
       ref: "User",
       required: [true, "Please provide a user (only employers can post jobs)"],
     },
+    experienceLevel: {
+      type: String,
+      enum: ["entry-level", "mid-level", "senior-level", "executive"],
+      required: false,
+    },
+    jobDuration: {
+      type: String,
+      enum: ["short-term", "long-term", "permanent"],
+    },
     datePosted: { type: Date, default: Date.now },
+    status: { type: String, enum: ["active", "closed"], default: "active" },
+    expiryDate: {
+      type: Date,
+      rerquired: false,
+    },
     reviews: [
       {
         type: mongoose.Schema.ObjectId,
